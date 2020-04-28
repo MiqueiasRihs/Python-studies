99b7de6d30e50dab9ad01070b271db272a5cb252
MiqueiasRihs <Miqueias.rihs@gmail.com>
2020-04-28 18:51:11 -0300

    Continuando os estudos

--- Estudando-Django/TreinaWeb/PythonDjango/gerenciador_tarefas/app/views.py
+++ Estudando-Django/TreinaWeb/PythonDjango/gerenciador_tarefas/app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .forms import TarefaForm
 
 # Create your views here.
 def listar_tarefas(request):
     nome_tarefa = "Assitir a semana Python e Django da TreinaWeb"
     return render(request, 'tarefas/listar_tarefas.html', {"nome_tarefa": nome_tarefa})
+
+def cadastrar_tarefa(request):
+    form_tarefa = TarefaForm()
+    return render(request, 'tarefas/form_tarefa.html', {"form_tarefa": form_tarefa})
