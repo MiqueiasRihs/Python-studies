417748434cf3dcd6aec979270cabf74ca1a803a6
MiqueiasRihs <Miqueias.rihs@gmail.com>
2020-05-04 20:09:15 -0300

    ss

--- Estudando-Django/TreinaWeb/PythonDjango/gerenciador_tarefas/app/urls.py
+++ Estudando-Django/TreinaWeb/PythonDjango/gerenciador_tarefas/app/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
     path('listar_tarefas/', listar_tarefas, name='listar_tarefas'),
     path('cadastrar_tarefas/', cadastrar_tarefa, name='cadastrar_tarefa'),
+    path('editar_tarefas/<int:id>', editar_tarefa, name='editar_tarefa'),
+    path('remover_tarefa/<int:id>', remover_tarefa, name='remover_tarefa'),
 
 ]
