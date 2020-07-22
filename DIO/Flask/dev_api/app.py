dfa0d7d5727822d096f6664533e40144d5c41d24
MiqueiasRihs <miqueias.rihs@gmail.com>
2020-07-22 08:20:27 -0300

    22-07

--- DIO/Flask/dev_api/app.py
+++ DIO/Flask/dev_api/app.py
@@ -14,17 +14,45 @@ desenvolvedores = [
     }
 ]
 
-
-@app.route('/dev/<int:id>/' methods=['GET', 'PUT'])
+## Devolve um desenvolvedor pelo ID.
+@app.route('/dev/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
 def desenvolvedor(id):
-    if request.method == 'GET'
-        desenvolvedor = desenvolvedores[id]
-        print(desenvolvedor)
+    if request.method == 'GET':
+        try:
+            response = desenvolvedores[id]
+        
+        except IndexError:
+            mensagem = 'Desenvolvedor de ID {} não existe'.format(id)
+            response = {'status':'erro', 'mensagem':mensagem}
+        
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status':'erro', 'mensagem':mensagem}
+
+        return jsonify(response)
+
     
-    elif request.method == 'PUT':
+    if request.method == 'PUT':
         dados = json.loads(request.data)
-        desenvolvedor[id] = dados
+        desenvolvedores[id] = dados
         return jsonify(dados)
+
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+  
+        return jsonify({'status':'sucesso', 'mensagem':'Registro excluido'})
+
+## Lista todos os desenvolvedores e permite registrar um novo desenvolvedor 
+@app.route('/dev/', methods=['POST', 'GET'])
+def lista_desenvolvedores():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        desenvolvedores.append(dados)
+        return jsonify({'status':'sucesso', 'mensagem':'Registro inserido'})
+
+    elif request.method == 'GET':
+        return jsonify(desenvolvedores)
+         
 
 
 if __name__ == '__main__':
