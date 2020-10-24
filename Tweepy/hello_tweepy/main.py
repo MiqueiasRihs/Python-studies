4fe519a95818356a3fd3d3c0b8bf02fbbc4085ef
MiqueiasRihs <miqueias.rihs@gmail.com>
2020-10-24 19:50:11 -0300

    hide tokens

--- Tweepy/hello_tweepy/main.py
+++ Tweepy/hello_tweepy/main.py
@@ -1,8 +1,14 @@
 import tweepy
+from .access_token import ACCESS_TOKENS
 
-auth = tweepy.OAuthHandler("asda", "asdas")
-auth.set_access_token("asda-asda", "asda")
+tokens = ACCESS_TOKENS
 
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(tokens["CONSUMER_KEY"], tokens["CONSUMER_SECRET"])
+auth.set_access_token(tokens["ACCESS_TOKEN"], tokens["ACCESS_TOKEN_SECRET"])
+
+# Create API object
 api = tweepy.API(auth)
 
+# Create a tweet
 api.update_status("Hello friend, this is a bot")
